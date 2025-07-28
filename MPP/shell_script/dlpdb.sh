13b10a420ea4c66311a559b05d76ab69e97e1d0c
chuckisbusy <chuckisbusy@icloud.com>
2025-07-28 08:12:07 -0400

    Create dlpdb.sh for OPM database

---
+++ MPP/shell_script/dlpdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+
+BASE_URL="https://opm-assets.storage.googleapis.com/pdb"
+out="/data/storage/cadeniran/pdbs"
+
+while IFS= read -r line; do
+    echo "Downloading from OPM database: $line.pdb"
+    url="$BASE_URL/$line.pdb"
+    wget $url -P $out -q
+done < pdbid
